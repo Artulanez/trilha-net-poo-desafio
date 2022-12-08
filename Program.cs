f913927280b59af08c0fcc825941f1646656fa24
Artulanez Souza <artulanez.luz@maida.health>
2022-12-08 18:47:50 -0300

    Implementação do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia!");
+
+Smartphone nokia = new Nokia("123456789", "Modelo 1", "11111111xxxx", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone!");
+Smartphone iphone = new Iphone("987654321", "Modelo 2", "2222222yyyy", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegran");
